ff03056d24175cd8ac92d817ce2d2c4f0fb70e7b
taznica <tailegog@gmail.com>
2020-12-31 23:36:12 +0900

    clean: rename ticket_view.dart to my_ticket_view.dart

--- lib/views/my_ticket/my_ticket_view.dart
+++ lib/views/my_ticket/my_ticket_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class TicketView extends StatelessWidget {
+class MyTicketView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
