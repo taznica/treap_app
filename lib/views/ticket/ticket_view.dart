2d4874026e3f1c3583d24f96c4ac5fdb73be6f2f
taznica <tailegog@gmail.com>
2021-01-01 23:50:54 +0900

    add: AppBar and base of list of tickets in ticket_view.dart

--- lib/views/ticket/ticket_view.dart
+++ lib/views/ticket/ticket_view.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:gohoubi_app/components/item_small_card.dart';
+import 'package:gohoubi_app/components/top_app_bar.dart';
+import 'package:gohoubi_app/models/user.dart';
 
 class TicketView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: TopAppBar(
+        title: 'チケット',
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ItemSmallCard(
+            item: user.tickets[index].item,
+            press: () {},
+          );
+        },
+        itemCount: user.tickets.length,
+      ),
+    );
   }
 }
