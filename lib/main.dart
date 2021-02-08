7c8beb1fed6cb2ecb2bd2702aeb0e23d257cb796
taznica <tailegog@gmail.com>
2021-02-08 17:49:33 +0900

    fix: use correct Japanese font by adding flutter_localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'views/home_tab_view.dart';
 
@@ -10,11 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const locale = Locale("ja", "JP");
+
     return MaterialApp(
       title: 'Gohoubi Alpha',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      locale: locale,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        locale,
+      ],
       home: HomeTabView(),
     );
   }
