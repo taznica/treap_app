31af3404ac60ab3d4e97eca74ff45e7adf9696a5
taznica <tailegog@gmail.com>
2021-02-13 01:41:24 +0900

    clean: add comma in message.dart

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,5 +1,10 @@
 class Message {
   final String text, image, sender, icon;
 
-  Message({this.text, this.image, this.sender, this.icon});
+  Message({
+    this.text,
+    this.image,
+    this.sender,
+    this.icon,
+  });
 }
